434691e5c7310f227be10cdcbc5df3ab49131b3c
Lacreation <lynearmandcreation@gmail.com>
2023-09-07 01:26:07 +0200

    [ADD] Results and freelances page

---
+++ src/pages/Freelances/index.jsx
@@ -0,0 +1,5 @@
+function Freelances() {
+  return <div>Freelances</div>
+}
+
+export default Freelances
---
+++ src/pages/Results/index.jsx
@@ -0,0 +1,5 @@
+function Results() {
+  return <div>Resultats</div>
+}
+
+export default Results
